35d1917099ade8d9b02ab209bd6ffa2020d2ba97
yappynoppy <hyano1991@gmail.com>
2019-04-07 01:38:18 +0900

    add tooltip

--- scatterplotzoom.js
+++ scatterplotzoom.js
@@ -1,11 +1,11 @@
-var checkbox = document.querySelector('#brush');
+var checkbox = document.querySelector("#brush");
 
-checkbox.addEventListener( 'change', function() {
-    if(this.checked) {
-      start_brush_tool()// Checkbox is checked..
-    } else {
-      end_brush_tool()// Checkbox is not checked..
-    }
+checkbox.addEventListener("change", function() {
+  if (this.checked) {
+    start_brush_tool(); // Checkbox is checked..
+  } else {
+    end_brush_tool(); // Checkbox is not checked..
+  }
 });
 
 // set the dimensions and margins of the graph
@@ -13,12 +13,20 @@ var margin = { top: 10, right: 30, bottom: 30, left: 60 },
   width = 460 - margin.left - margin.right,
   height = 400 - margin.top - margin.bottom;
 
+// Set the zoom and Pan features: how much you can zoom, on which part, and what to do when there is a zoom
+var zoom = d3
+  .zoom()
+  //.scaleExtent([1, 1]) // This control how much you can unzoom (x0.5) and zoom (x20)
+  //.translateExtent([[-100, -100], [height + 100, width + 100]])
+  .on("zoom", zoomed);
+
 // append the SVG object to the body of the page
 var SVG = d3
   .select("#dataviz_axisZoom")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
+  .call(zoom)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -35,7 +43,10 @@ var y = d3
   .domain(y0)
   .range([height, 0]);
 
-var brush = d3.brush().on("end", brushended),
+var brush = d3
+    .brush()
+    .extent([[0, 0], [width, height]])
+    .on("end", brushended),
   idleTimeout,
   idleDelay = 350;
 
@@ -60,23 +71,7 @@ var clip = SVG.append("defs")
 // Create the scatter variable: where both the circles and the brush take place
 var scatter = SVG.append("g").attr("clip-path", "url(#clip)");
 
-// Set the zoom and Pan features: how much you can zoom, on which part, and what to do when there is a zoom
-var zoom = d3
-  .zoom()
-  //.scaleExtent([0.5, 20]) // This control how much you can unzoom (x0.5) and zoom (x20)
-  //.extent([[0, 0], [width, height]])
-  .on("zoom", zoomed);
-
-// This add an invisible rect on top of the chart area. This rect can recover pointer events: necessary to understand when the user zoom
-SVG.append("rect")
-  .attr("width", width)
-  .attr("height", height)
-  .style("fill", "none")
-  .style("pointer-events", "all")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-  .call(zoom);
-
-function updateChart(newX, newY){
+function updateChart(newX, newY) {
   var t = SVG.transition().duration(750);
 
   // update axes with these new boundaries
@@ -100,22 +95,21 @@ function updateChart(newX, newY){
 function zoomed() {
   // recover the new scale
   var newX = d3.event.transform.rescaleX(x);
-  var newY = d3.event.transform.rescaleY(y); 
-  
-      // update axes with these new boundaries
-      xAxis.call(d3.axisBottom(newX));
-      yAxis.call(d3.axisLeft(newY));
-    
-      // update circle position
-      scatter
-        .selectAll("circle")
-        .attr("cx", function(d) {
-          return newX(d.Sepal_Length);
-        })
-        .attr("cy", function(d) {
-          return newY(d.Petal_Length);
-        });
+  var newY = d3.event.transform.rescaleY(y);
 
+  // update axes with these new boundaries
+  xAxis.call(d3.axisBottom(newX));
+  yAxis.call(d3.axisLeft(newY));
+
+  // update circle position
+  scatter
+    .selectAll("circle")
+    .attr("cx", function(d) {
+      return newX(d.Sepal_Length);
+    })
+    .attr("cy", function(d) {
+      return newY(d.Petal_Length);
+    });
 }
 
 function idled() {
@@ -132,8 +126,9 @@ function brushended() {
     newX = x.domain(x0);
     newY = y.domain(y0);
   } else {
-    newX = x.domain([s[0][0], s[1][0]].map(x.invert, x));
-    newY = y.domain([s[1][1], s[0][1]].map(y.invert, y));
+    newX = x.domain([s[0][0], s[1][0]].map(x.invert));
+    newY = y.domain([s[1][1], s[0][1]].map(y.invert));
+
     SVG.select(".brush").call(brush.move, null);
   }
   updateChart(newX, newY);
@@ -153,6 +148,49 @@ function end_brush_tool() {
   SVG.selectAll("g.brush").remove();
 }
 
+var tooltip = d3
+  .select("#dataviz_axisZoom")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+// tooltip mouseover event handler
+var tipMouseover = function(d) {
+  //var color = colorScale(d.manufacturer);
+  var html =
+    "Sepal_Length " +
+    "<b>" +
+    d.Sepal_Length +
+    "</b>" +
+    "<br>" +
+    "Petal_Length " +
+    "<b>" +
+    d.Petal_Length +
+    "</b>";
+
+  tooltip
+    .html(html)
+    .style("left", d3.event.pageX + 15 + "px")
+    .style("top", d3.event.pageY - 28 + "px")
+    .transition()
+    .duration(200) // ms
+    .style("opacity", 0.9); // started as 0!
+};
+// tooltip mouseout event handler
+var tipMouseout = function(d) {
+  tooltip
+    .transition()
+    .duration(300) // ms
+    .style("opacity", 0); // don't care about position!
+};
+
+function reset_zoom() {
+  newX = x.domain(x0);
+  newY = y.domain(y0);
+
+  updateChart(newX, newY);
+}
+
 d3.csv(
   "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv",
   function(data) {
@@ -170,14 +208,20 @@ d3.csv(
       })
       .attr("r", 8)
       .style("fill", "#61a3a9")
-      .style("opacity", 0.5);
+      .style("opacity", 0.5)
+      .on("mouseover", tipMouseover)
+      .on("mouseout", tipMouseout);
   }
 );
 
-function reset_zoom(){
-  newX = x.domain(x0);
-  newY = y.domain(y0);
+// This add an invisible rect on top of the chart area. This rect can recover pointer events: necessary to understand when the user zoom
+//d3.select("#dataviz_axisZoom")
+/*SVG.append("rect")
+  .attr("width", width)
+  .attr("height", height)
+  .style("fill", "none")
+  .style("pointer-events", "all")
+  //.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+  .call(zoom);*/
 
-  updateChart(newX, newY);
-  
-};
+//SVG.call(zoom);
